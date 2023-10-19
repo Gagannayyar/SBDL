8aa9c767bf500c5383a9c729d7ae9a41753e5426
Gagan Nayyar <gagan.nayyar@outlook.com>
2023-10-19 10:20:42 +0000

    Basic template without unit test

--- lib/DataLoader.py
+++ lib/DataLoader.py
@@ -1,12 +1,61 @@
-import configparser
-from pyspark import SparkConf
+from lib import ConfigLoader
+
+def get_account_schema():
+    schema = """load_date date,active_ind int,account_id string,
+        source_sys string,account_start_date timestamp,
+        legal_title_1 string,legal_title_2 string,
+        tax_id_type string,tax_id string,branch_code string,country string"""
+    return schema
 
 
-def get_config(env):
-    config = configparser.ConfigParser()
-    config.read("conf/sbdl.conf")
-    conf = {}
-    for key, value in config.items(env):
-        conf[key] = value
+def get_party_schema():
+    schema = """load_date date,account_id string,party_id string,
+    relation_type string,relation_start_date timestamp"""
+    return schema
 
-    return conf
+
+def get_address_schema():
+    schema = """load_date date,party_id string,address_line_1 string,
+    address_line_2 string,city string,postal_code string,
+    country_of_address string,address_start_date date"""
+    return schema
+
+def read_accounts(env, spark, hive_db, enable_hive):
+    runtime_filter = ConfigLoader.get_data_filter(env, "account.filter")
+
+    if enable_hive:
+        return spark.read("select * from" + hive_db + ".accounts").where(runtime_filter)
+
+    else:
+        return spark.read \
+            .format("csv") \
+            .option("headers", "true") \
+            .schema(get_account_schema()) \
+            .load("data/account_samples.csv") \
+            .where(runtime_filter)
+
+def read_party(env, spark, hive_db, enable_hive):
+    runtime_filter = ConfigLoader.get_data_filter(env, "party.filter")
+
+    if enable_hive:
+        return spark.read("select * from" + hive_db + ".parties").where(runtime_filter)
+
+    else:
+        return spark.read \
+            .format("csv") \
+            .option("headers", "true") \
+            .schema(get_party_schema()) \
+            .load("data/party_samples.csv") \
+            .where(runtime_filter)
+
+def read_address(spark, env, enable_hive, hive_db):
+    runtime_filter = ConfigLoader.get_data_filter(env, "address.filter")
+    if enable_hive:
+        return spark.sql("select * from " + hive_db + ".party_address").where(runtime_filter)
+    else:
+        return spark.read \
+            .format("csv") \
+            .option("header", "true") \
+            .schema(get_address_schema()) \
+            .load("test_data/address_samples.csv") \
+            .where(runtime_filter)
